bb1509f55fb997ab0630a3299cc9fd4298d37c13
rismanitalst <rismanitalst@gmail.com>
2026-06-26 16:07:17 +0700

    buat folder android, tambahkan firebase ke dompet kampus global

---
+++ dompet_kampus_global/android/app/src/main/kotlin/com/example/nitadmptdgtl/dompet_kampus_global/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.nitadmptdgtl.dompet_kampus_global
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- dompet_kampus_global/lib/main.dart
+++ dompet_kampus_global/lib/main.dart
@@ -1,14 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'core/router/app_router.dart';
 import 'core/theme/app_theme.dart';
 import 'core/utils/app_bloc_observer.dart';
+import 'firebase_options.dart';
 import 'injection/injection_container.dart' as di;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   Bloc.observer = const AppBlocObserver();
 
   // Initialize dependency injection
